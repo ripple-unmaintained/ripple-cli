c43c89de7a84b9ee5aa671b7fcaf444d7e1e2c25
Steven Zeiler <zeiler.steven@gmail.com>
2015-05-31 21:27:23 -0700

    [FEATURE] Register global commander options

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+
+var commander = require('commander')
+
+commander
+  .version('0.1.0')
+  .option('-a --account <account>', 'ripple account')
+  .option('-k --key-path <path>', 'path to account secret key')
+  .parse(process.argv)
+
